f0f7fbb4277f2016bbd840ad345dd0ddb35730b6
AwesomeAim <30537683+AwesomeAim@users.noreply.github.com>
2023-11-12 01:58:22 -0800

    updating realtime part of script

--- database/processing/commodities_db_script.py
+++ database/processing/commodities_db_script.py
@@ -20,8 +20,10 @@ def load(path):
         exit(1)
 
 def main():
-    # load json
-    data = load('../../Data_Collection/Output/Unified_Commodities_Output.json')
+    # load json 
+    # idk what this will be called. update when able
+    # variable setting may have to be adjusted too
+    data = load('../../data_collection/output/unified_commodities_output.json')
 
     try:
         # create with context manager
@@ -30,26 +32,35 @@ def main():
                 symbol = entry['symbol']
                 name = entry['name']
                 # establish if it exists already
-                result = conn.execute(text(f"select ID from `Commodity_List` where Symbol = '{symbol}'"))
+                result = conn.execute(text(f"select id from `commodities` where symbol = '{symbol}'"))
                 row = result.one_or_none()
                 if row is None:
                     # execute plain sql insert statement - transaction begins
-                    conn.execute(text(f"insert into `Commodity_List`(`ID`, `Name`, `Symbol`) values (NULL, '{name}', '{symbol}')"))
+                    conn.execute(text(f"insert into `commodities`(`id`, `commodityName`, `symbol`) values (NULL, '{name}', '{symbol}')"))
                     conn.commit()
                     # get the generated ID
-                    result = conn.execute(text(f"select ID from `Commodity_List` where Symbol = '{symbol}'")) 
+                    result = conn.execute(text(f"select id from `commodities` where symbol = '{symbol}'")) 
                     CommodityID = result.one()[0]
                 else:
                     CommodityID = row[0]
                     
+                # putting data into variables (I really hate this)
                 date = datetime.datetime.fromtimestamp(entry['realtime_data']['timestamp']) 
+                price = entry['realtime_data']['price']
+                changePercent = entry['realtime_data']['changePercentage']
+                change = entry['realtime_data']['change']
+                dayHigh = entry['realtime_data']['dayHigh']
+                dayLow = entry['realtime_data']['dayLow']
+                yearHigh = entry['realtime_data']['yearHigh']
+                yearLow = entry['realtime_data']['yearLow']
+                marketCap = entry['realtime_data']['mktCap']
+                exchange = entry['realtime_data']['exchange']
                 commodityOpen = entry['realtime_data']['open']
-                high = entry['realtime_data']['dayHigh']
-                low = entry['realtime_data']['dayLow']
-                #close = entry['realtime_data']['previousClose'] no close
+                close = entry['realtime_data']['previousClose']
                 volume = entry['realtime_data']['volume']
+                volumeAverage = entry['realtime_data']['volAvg']
                 try:
-                    conn.execute(text(f"insert into `Commodity_Values`(`CommodityID`, `Date`, `Open`, `High`, `Low`, `Close`, `Volume`) values ('{CommodityID}', '{date}', '{commodityOpen}', '{high}', '{low}', null, '{volume}')"))
+                    conn.execute(text(f"insert into `realtime_commodity_values`(`commodity_id`, `date`, `price`, `changePercentage`, `change`, `dayHigh`, `dayLow`, `yearHigh`, `yearLow`, `mktCap`, `exchange`, `open`, `prevClose`, `volume`, `volAvg`) values ('{CommodityID}', '{date}', '{price}', '{changePercent}', '{change}', '{dayHigh}', '{dayLow}', '{yearHigh}', '{yearLow}', '{marketCap}', '{exchange}', '{commodityOpen}', '{close}', '{volume}', '{volumeAverage}')"))
                     conn.commit()
                 except IntegrityError as e: # catch duplicate entry
                     volume = volume # do nothing
