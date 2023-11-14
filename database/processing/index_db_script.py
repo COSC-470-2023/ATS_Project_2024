bbbfa09756a963af38af14cd26da53bcee4585ad
Parker Green <parkerfishergreen@gmail.com>
2023-11-14 13:54:52 -0800

    Updated index_db_script.py and connect.py
    updated values and tested. fixed typo in connect.py

--- database/processing/index_db_script.py
+++ database/processing/index_db_script.py
@@ -6,7 +6,7 @@ import traceback
 from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError
 
-def load(path):
+def load_output_file(path):
     try:
         with open(path, "r") as output_file:
             output_data = json.load(output_file)
@@ -20,58 +20,74 @@ def load(path):
 
 def main():
     # load json
-    data = load('../../Data_Collection/Output/Unified_Index_Output.json')
+    realtime_data = load_output_file('./data_collection/output/raw_index_output.json')
+    #historical_data = load_output_file('../../data_collection/output/historical_index_output.json')
 
     try:
         # create with context manager
         with connect.connect() as conn:
-             for entry in data:
+            for entry in realtime_data:
                 symbol = entry['symbol']
                 name = entry['name']
-                # check if index exists in indices table
-                ##Needs to be changed
-                result = conn.execute(text(f"select ID from `Indices` where Symbol = '{symbol}'"))
+
+                # check if index exists in indexes table
+                result = conn.execute(text(f"SELECT id FROM `indexes` WHERE symbol = '{symbol}'"))
                 row = result.one_or_none()
+
                 if row is None:
                     # execute plain sql insert statement - transaction begins
-                    ##Needs to be updated
-                    conn.execute(text(f"insert into `Indices`(`ID`, `Name`, `Symbol`) values (NULL, '{name}', '{symbol}')"))
+                    conn.execute(text(f"INSERT INTO `indexes`(`indexname`, `symbol`) VALUES ('{name}', '{symbol}')"))
                     conn.commit()
+
                     # get the generated ID
-                    ##Needs to be updated
-                    result = conn.execute(text(f"select ID from `Indices` where Symbol = '{symbol}'")) 
-                    IndexID = result.one()[0]
+                    result = conn.execute(text(f"SELECT id FROM `indexes` WHERE symbol = '{symbol}'")) 
+                    index_id = result.one()[0]
                 else:
-                    IndexID = row[0]
+                    index_id = row[0]
+                    
                 # process realtime data
-                ##Needs to be updated from realtime_commoditiy_values
-                date = datetime.datetime.fromtimestamp(entry['realtime_data']["timestamp"])
-                indexOpen = entry['realtime_data']['open']
-                high = entry['realtime_data']['dayHigh']
-                low = entry['realtime_data']['dayLow']
-                #close = entry['realtime_data']['close']
-                volume = entry['realtime_data']['volume']
+                date = datetime.datetime.fromtimestamp(entry["timestamp"])
+                price = entry['price']
+                change_percentage = entry['changesPercentage']
+                change = entry['change']
+                day_high = entry['dayHigh']
+                day_low = entry['dayLow']
+                year_high = entry['yearHigh']
+                year_low = entry['yearLow']
+                mkt_cap = entry['marketCap'] # Remove from db, seems to be null in all cases.
+                exchange = entry['exchange']
+                open_price = entry['open']
+                prev_close = entry['previousClose']
+                volume = entry['volume']
+                vol_avg = entry['averageVolume']
+
                 try:
-                    ##Needs to be updated
-                    conn.execute(text(f"insert into `Index_Values`(`IndexID`, `Date`, `Open`, `High`, `Low`, `Close`, `Volume`) values ('{IndexID}', '{date}', '{indexOpen}', '{high}', '{low}', null, '{volume}')"))
+                    conn.execute(text(f"INSERT INTO `realtime_index_values` VALUES ('{index_id}', '{date}', '{price}', '{change_percentage}', '{change}', '{day_high}', '{day_low}', '{year_high}', '{year_low}', 0, '{exchange}','{open_price}', '{prev_close}', '{volume}', '{vol_avg}')"))
                     conn.commit()           
                 except IntegrityError as e:
-                    volume = volume # do nothing  
+                    continue
 
-                # process historical data
-                #Needs to be updated
-                for h_entry in entry['historical_data']:
-                    date = h_entry['date']
-                    indexOpen = h_entry['open']
-                    high = h_entry['high']
-                    low = h_entry['low']
-                    close = h_entry['close']
-                    volume = h_entry['volume']
-                    try: 
-                        conn.execute(text(f"insert into `Index_Values`(`IndexID`, `Date`, `Open`, `High`, `Low`, `Close`, `Volume`) values ('{IndexID}', '{date}', '{indexOpen}', '{high}', '{low}', '{close}', '{volume}')"))
-                        conn.commit()
-                    except IntegrityError as e: # catch duplicate entries
-                        continue
+            # historical insertions may need to be handled in a different script
+
+            # process historical data
+            # for h_entry in historical_data:
+            #     date = h_entry['date']
+            #     index_open = h_entry['open']
+            #     high = h_entry['high']
+            #     low = h_entry['low']
+            #     close = h_entry['close']
+            #     adj_close = h_entry['adjClose']
+            #     volume = h_entry['volume']
+            #     unadjusted_volume = h_entry['unadjustedVolume']
+            #     change = h_entry['change']
+            #     change_percentage = h_entry['changePercentage']
+            #     vwap = h_entry['vwap']
+            #     change_over_time = h_entry['changeOverTime']
+            #     try: 
+            #         conn.execute(text(f"INSERT INTO `historical_index_values` VALUES ('{index_id}', '{date}', '{index_open}', '{high}', '{low}', '{close}','{adj_close}', '{volume}', '{unadjusted_volume}', '{change}', '{change_percentage}', '{vwap}', '{change_over_time}')"))
+            #         conn.commit()
+            #     except IntegrityError as e: # catch duplicate entries
+            #         continue
                 
     except Exception as e:
         print(e)
