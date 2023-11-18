612561c7cc939e0a4c4bda3fcf5835e2fb280fed
MarcoDelMondo <b.carrier373@gmail.com>
2023-11-17 20:01:55 -0800

    Implement code review
    Implemented XP code Review changes, updated importing connect

--- database/processing/symbol_change_update.py
+++ database/processing/symbol_change_update.py
@@ -1,26 +1,28 @@
-import connect
 import json
 import traceback
 from sqlalchemy import text
 
+from database.processing import connect
 
-def load(path):
+
+def load_output_file(path):
     try:
         with open(path, "r") as output_file:
-            if output_file.read(2) == '[]':
-                output_file.seek(0)
+            output_data = json.load(output_file)
+            if not output_data:
                 print("No symbol change update required")
                 exit(0)
-            output_data = json.load(output_file)
         return output_data
     except FileNotFoundError:
         print(f"Error: Output file '{path}' not found.")
         exit(1)
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
         print(f"Error decoding JSON in '{path}'.")
+        print(e)
         exit(1)
 
-def update(conn, symbol):
+
+def update_symbol(conn, symbol):
     try:
         # Variable Declarations
         date = symbol["date"]
@@ -29,25 +31,25 @@ def update(conn, symbol):
         new_symbol = symbol["newSymbol"]
 
         #  SQL query
-        company_update = text("UPDATE Companies SET companyName = %s, symbol = %s WHERE company_id IN (SELECT company_id FROM Companies WHERE symbol = %s)")
-        # Use a tuple to parametize variables (prevent injection)
-        data = (name, old_symbol, new_symbol)
+        company_update = text(f"UPDATE Companies SET companyName = '{name}', symbol = '{new_symbol}' WHERE symbol = '{old_symbol}'")
+        # Use a tuple to parameterize variables (prevent injection)
+        # data = (name, old_symbol, new_symbol)
 
-        conn.execute(company_update, data)
+        conn.execute(company_update)
         conn.commit()
     except Exception as e:
         print(f"Error in updating database: {e}")
         print(traceback.format_exc())
+
+
 def main():
     try:
-        # Load database credentials from the config file
-        config = load("config.json")
         # Establish a connection to server
-        with connect.connect(**config) as conn:
-            symbol_change = load("data_collection/output/dummy_output_file.json")
+        with connect.connect() as conn:
+            symbol_change = load_output_file(r"C:\Users\BCarr\Documents\GitHub\SMF_Project_2023\data_collection\output\dummy_output_file.json")
 
             for symbol in symbol_change:
-                update(conn, symbol)
+                update_symbol(conn, symbol)
     except Exception as e:
         print(e)
         print(traceback.format_exc())
