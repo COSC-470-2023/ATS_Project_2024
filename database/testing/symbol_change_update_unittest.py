c1a5b5f5ceaa1fa68e37642e82db2b0e987b1336
JRawlings66 <jacobrawlings66@gmail.com>
2023-11-17 17:10:09 -0800

    Update symbol_change_update_unittest.py

--- database/testing/symbol_change_update_unittest.py
+++ database/testing/symbol_change_update_unittest.py
@@ -3,5 +3,8 @@ import json
 import connect
 import traceback
 
+from processing.symbol_change_update import main
 
 class SymbolChangeTest(unittest.TestCase):
+    @patch("symbol_change_query.load")
+    def test_update(self):
