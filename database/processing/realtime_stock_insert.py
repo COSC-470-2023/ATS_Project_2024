550ab618ab9f7dddeb5cb6ecfcf2905983628c46
dakota <40772063+irradiated@users.noreply.github.com>
2023-11-19 13:41:01 -0800

    work

--- database/processing/realtime_stock_insert.py
+++ database/processing/realtime_stock_insert.py
@@ -20,6 +20,66 @@ def load_output_file(path):
         print(f"Error decoding JSON in '{path}'")
         exit(1)
 
+def execute_insert(connection, entry, company_id):
+    date = datetime.datetime.fromtimestamp(entry["_realtime_date"])
+    price = entry['_realtime_price']
+    change_percentage = entry['_realtime_changesPercent']
+    change = entry['_realtime_change']
+    day_high = entry['_realtime_dayHigh']
+    day_low = entry['_realtime_dayLow']
+    year_high = entry['_realtime_yearHigh']
+    year_low = entry['_realtime_yearLow']
+    mkt_cap = entry['_realtime_mktCap']
+    exchange = entry['_realtime_exchange']
+    open_price = entry['_realtime_openPrice']
+    prev_close = entry['_realtime_prevClose']
+    volume = entry['_realtime_volume']
+    vol_avg = entry['_realtime_volAvg']
+    # eps
+    # pe
+    # earningsAnnouncement
+    # sharesOutstanding
+    # Execute row insertion
+    connection.execute(text(f"INSERT INTO `real_time_stock_values` VALUES ('{company_id}', '{date}', '{price}', '{change_percentage}', '{change}', '{day_high}', '{day_low}', '{year_high}', '{year_low}', '{mkt_cap}, '{exchange}','{open_price}', '{prev_close}', '{volume}', '{vol_avg}')"))
+    connection.commit()
+
+def get_company_id(entry, conn):
+    symbol = entry['symbol']
+    name = entry['name']
+    # check if index exists in indexes table
+    result = conn.execute(text(f"SELECT id FROM `companies` WHERE symbol = '{symbol}'"))
+    row = result.one_or_none()
+
+    if row is None:
+        # execute plain sql insert statement - transaction begins
+        conn.execute(text(f"INSERT INTO `companies`(`companyName`, `symbol`) VALUES ('{name}', '{symbol}')"))
+        conn.commit()
+        # get the generated ID
+        result = conn.execute(text(f"SELECT id FROM `companies` WHERE symbol = '{symbol}'")) 
+        company_id = result.one()[0]
+    else:
+        company_id = row[0] 
+    return company_id
+
+def main():
+    # Load json data
+    realtime_data = load_output_file('./test_files/static_test_files/static_index_realtime.json')
+
+    try:
+        # create with context manager
+        with connect.connect() as conn:
+            for entry in realtime_data:
+                index_id = get_company_id(entry, conn)
+                try:    
+                    # process realtime data
+                    execute_insert(conn, entry, index_id)
+                except IntegrityError as e:
+                    continue
+
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+        print("SQL connection error")
 
 def main():
     # Load json data
