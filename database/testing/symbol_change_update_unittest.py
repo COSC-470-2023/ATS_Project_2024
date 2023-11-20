60d0528a250691e55ea6f1d3c4d3a0df3d052c0d
MarcoDelMondo <b.carrier373@gmail.com>
2023-11-19 16:45:11 -0800

    Completed Unittest functionality

--- database/testing/symbol_change_update_unittest.py
+++ database/testing/symbol_change_update_unittest.py
@@ -1,49 +1,45 @@
 import unittest
-import json
-import traceback
-import sys
-from sqlalchemy import text
-
-from database.processing import symbol_change_update, connect, credentials
+from database.processing import connect
 from database.processing.symbol_change_update import load_output_file, update_symbol
 
 
 class SymbolChangeTest(unittest.TestCase):
-    def createTestDB(self):
-        # connect to the test database
-        self.conn = connect.connect()
-        # create a test Companies table
-        create = text(f"CREATE TABLE `CompaniesTest` (`id` BIGINT, `companyName` VARCHAR(300) NOT NULL, `symbol` VARCHAR(10) NOT NULL, `isListed` boolean, PRIMARY KEY (`id`))")
-        with self.conn as conn:
-            conn.execute(create)
-
-    def dropTestDB(self):
-        # drop test table
-        self.conn.execute("DROP TABLE CompaniesTest")
-        # Close the connection
-        self.conn.close()
-
     def test_update_symbol(self):
-        self.createTestDB()
-        # load test data
-        test1 = load_output_file(r"test1.json")
-        test2 = load_output_file(r"test2.json")
-        test3 = load_output_file(r"test3.json")
+        try:
+            # Load test data
+            test1 = load_output_file(r"test1.json")
+            test2 = load_output_file(r"test2.json")
+            test3 = load_output_file(r"test3.json")
 
-        #check to see if the loaded test data is a proper list
-        self.assertIsInstance(test1, list)
-        self.assertIsInstance(test2, list)
-        self.assertIsInstance(test3, list)
+            # Check to see if the loaded test data is a proper list
+            self.assertIsInstance(test1, list)
+            self.assertIsInstance(test2, list)
+            self.assertIsInstance(test3, list)
 
-        #check if the update_symbol function runs without errors. function should return nothing if no errors occured
-        with self.subTest("Test one"):
-            self.assertIsNone(update_symbol(self.conn, test1[0]))
-        with self.subTest("Test two"):
-            self.assertIsNone(update_symbol(self.conn, test2[0]))
-        with self.subTest("Test three"):
-            self.assertIsNone(update_symbol(self.conn, test3[0]))
-        self.dropTestDB()
+            # Check if the update_symbol function runs without errors. function should return nothing if no errors occured
+            with connect.connect() as conn:
+                with self.subTest("Test one"):
+                    print("Starting Test 1...")
+                    for symbol in test1:
+                        print("Symbol Changed: ", symbol)
+                        self.assertIsNone(update_symbol(conn, test1[0]))
+                    print("Test 1 Complete ✔")
+                with self.subTest("Test two"):
+                    print("Starting Test 2...")
+                    for symbol in test2:
+                        print("Symbol Changed: ", symbol)
+                        self.assertIsNone(update_symbol(conn, test2[0]))
+                    print("Test 2 Complete ✔")
+                with self.subTest("Test three"):
+                    print("Starting Test 3...")
+                    for symbol in test3:
+                        print("Symbol Changed: ", symbol)
+                        self.assertIsNone(update_symbol(conn, test3[0]))
+                    print("Test 3 Complete ✔")
+
+        except Exception as e:
+            print(e)
+
 
 if __name__ == '__main__':
     unittest.main()
-
