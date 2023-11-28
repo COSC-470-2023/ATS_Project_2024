b0e348f9346f6778764ec73ef82dc98df4ac2ab3
Jacob Labelle <jacob.labelle93@gmail.com>
2023-11-28 00:25:39 -0800

    Modified symbol_change_query.py
    Added function to backup config file before changes.
    
    Added functionality for iterating over a list of defined configuration files.
    
    Made task completion more verbose for logging purposes.

--- data_collection/collection/symbol_change_query.py
+++ data_collection/collection/symbol_change_query.py
@@ -1,7 +1,8 @@
 # Get data from the symbol change API list file
 # Parse the json so we know what our URL and keys are
 # Iterate through the stocks using the key and url, then move onto the next API
-# TODO Add a function to make a backup of the system configuration before writing changes
+# Backup and modify config files
+# Write an output file in the expected system format from the API queries
 
 import requests
 from datetime import date
@@ -52,7 +53,7 @@ def trim_query_output(raw_API_output):
 # Modify the structure of the symbol change list to include oldName as a field and maps field names to expected output names
 # TODO Pull oldName from system_config
 # NOTE Consider how multiple API sources are handled in this process
-def modify_output_list(symbol_change_list, system_config):
+def modify_output_list(symbol_change_list):
     modified_symbols_list = []
     for entry in symbol_change_list:
         date = entry['date']
@@ -75,6 +76,7 @@ def modify_system_config(system_config_json):
         # For each key in dictionary
         for key, value in entry.items():
             # If the key matches the specified condition, enter and assess if the symbol value is in the changelog
+            # TODO Find a way to refactor this
             if key == 'index_composites':
                 for index in value:
                     if index['symbol'] in symbol_changelog:
@@ -87,13 +89,19 @@ def modify_system_config(system_config_json):
                 for commodity in value:
                     if commodity['symbol'] in symbol_changelog:
                         commodity['symbol'] = symbol_changelog[commodity['symbol']]
+            elif key == 'companies':
+                for company in value:
+                    if company['symbol'] in symbol_changelog:
+                        company['symbol'] = symbol_changelog[company['symbol']]               
             else:
                 continue
     return modified_system_config
 
 
 def main():
-    symbol_query_config = JsonModifier.load_config("../configuration/symbol_change_query_cfg.json")
+    # Load the symbol change query config
+    symbol_change_query_config_path = "../configuration/symbol_change_query_cfg.json"
+    symbol_query_config = JsonModifier.load_config(symbol_change_query_config_path)
     raw_API_query_output = []
     symbol_change_output = []
     # Iterate through each API in the list
@@ -107,17 +115,28 @@ def main():
     # If no symbols have changed, proceed to final activities
     if symbol_changed:
         print('Symbols changed. Performing system change tasks.')
-        system_config = JsonModifier.load_config("../configuration/realtime_query_cfg.json")
         # Write modified output to symbol_change file
-        symbol_change_output = modify_output_list(modified_API_output, system_config)
-        # Modify changed symbols in system config file
-        modified_system_config = modify_system_config(system_config)
-        # Write changes to system config file
-        JsonModifier.write_files(modified_system_config, "../configuration/", "realtime_query_cfg.json")
-    # Write empty list, or changedlist to output file, exit with success code
-    print('Task complete.')
-    # write_output_file(symbol_change_output)
+        symbol_change_output = modify_output_list(modified_API_output)
+        # Variables for use with the system config tasks
+        system_config_path_list = ["../configuration/realtime_query_cfg.json", 
+                                   "../configuration/company_info_query_cfg.json",
+                                   "../configuration/historical_query_cfg.json"]
+        config_path = "../configuration/"
+        config_backup_path = "../configuration/backup/"
+        for system_config_path in system_config_path_list:
+            system_config = JsonModifier.load_config(system_config_path)
+            # Pull config file name from path
+            system_config_name = system_config_path.split("/")[-1]
+            # Write a backup config file to the backup directory
+            system_config_name_backup = system_config_name + "_" + str(date.today()) + "~"
+            JsonModifier.write_files(system_config, config_backup_path, system_config_name_backup)
+            # Modify changed symbols in system config file
+            modified_system_config = modify_system_config(system_config)
+            # Write changes to system config file
+            JsonModifier.write_files(modified_system_config, config_path, system_config_name)
+    # Write empty list, or changedlist to output file, exit with success code 
     JsonModifier.write_files(symbol_change_output, "../output/", "symbol_change_list.json")
+    print(f'Task complete. Symbols changed for ' + str(date.today()) + ': ' + str(symbol_changed))
     exit(0)
 
 
