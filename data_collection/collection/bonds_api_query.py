3e932ed0f3b39e61bd34c2c391ed2358d94e7213
JRawlings66 <jacobrawlings66@gmail.com>
2023-11-29 16:35:30 -0800

    unneeded for loop removed, unneeded methods removed and using JsonHandler now, Fixed duplicate day bug

--- data_collection/collection/bonds_api_query.py
+++ data_collection/collection/bonds_api_query.py
@@ -1,10 +1,12 @@
+import json
+
 import requests
 from datetime import date, timedelta
 from JsonHandler import JsonHandler
 
 # Globals
-BONDS_CFG_PATH = "./data_collection/configuration/bonds_query_cfg.json"
-OUTPUT_FOLDER = "./data_collection/output/"
+BONDS_CFG_PATH = "C:/Users/Jacob/PycharmProjects/SMF_Project_2023/data_collection/configuration/bonds_query_cfg.json"
+OUTPUT_FOLDER = "C:/Users/Jacob/PycharmProjects/SMF_Project_2023/data_collection/output"
 OUTPUT_FILENAME_BONDS = "bonds_output.json"
 
 
@@ -25,7 +27,7 @@ def create_date_window(days_queried):
         start = end - timedelta(days=90)
         window = {start: end}
         date_windows.append(window)
-        end = start
+        end = start - timedelta(days=1)
     # Handle remainder (days_queried not divisible by 90)
     if rem > 0:
         start = end - timedelta(days=rem)
@@ -53,31 +55,29 @@ def make_queries(api_url, api_key, api_fields, treasuries, non_api_fields, days_
 
         response = requests.get(query)
         # convert the response to json and append to list
-        data = response.json()
+        bonds_output = json.loads(response.text)
 
-        for entry in data:
+        for data in bonds_output:
             # Remap the field names to the config, using the values from API fields,
             # as the key, and the values of entry as the values of the new entry.
-            entry = dict((zip(api_fields.values(), list(entry.values()))))
+            entry = dict((zip(api_fields.values(), list(data.values()))))
 
             # Get name from the config
-            if non_api_fields != {}:  # There is a manually added field in the cfg.
-                for non_api_field in non_api_fields:
-                    # Compare the manually added field to where it should get its data from
-                    # in the normal fields
-                    try:
-                        src = non_api_fields[non_api_field]['src']
-                        map_to = non_api_fields[non_api_field]['mapping']
-                        input_type = non_api_fields[non_api_field]['input_type']
-                        output_type = non_api_fields[non_api_field]['output_type']
-                        # Handler for grabbing the name of treasury from config.
-                        # This also can handler conversion, so It's needed to not just doa  static mapping in the future.
-                        if input_type == "_string" and output_type == "_string":
-                            if src == "_config_name":
-                                entry[map_to] = treasuries[0]['name']
-                    except KeyError as e:
-                        print(f"Key Error on api {iter(entry)}:\n{e}")
-                        pass  # TODO make log files entry
+            # Compare the manually added field to where it should get its data from
+            # in the normal fields
+            try:
+                src = non_api_fields['name']['src']
+                map_to = non_api_fields['name']['mapping']
+                input_type = non_api_fields['name']['input_type']
+                output_type = non_api_fields['name']['output_type']
+                # Handler for grabbing the name of treasury from config.
+                # This also can handler conversion, so It's needed to not just doa  static mapping in the future.
+                if input_type == "_string" and output_type == "_string":
+                    if src == "_config_name":
+                        entry[map_to] = treasuries[0]['name']
+            except KeyError as e:
+                print(f"Key Error on api {iter(entry)}:\n{e}")
+                pass  # TODO make log files entry
 
             # Append the modified entry to the output
             bonds_data.append(entry)
@@ -100,7 +100,7 @@ def main():
         #   Should be done in a way that an API that doesnt only return one treasury can use it.
         treasuries = bond_config[api]['treasuries']
 
-        output += make_queries(url, key, api_fields, treasuries, non_api_fields, days_queried)
+        output = make_queries(url, key, api_fields, treasuries, non_api_fields, days_queried)
 
     JsonHandler.write_files(output, OUTPUT_FOLDER, OUTPUT_FILENAME_BONDS)
 
