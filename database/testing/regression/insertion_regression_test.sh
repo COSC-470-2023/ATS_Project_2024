60aae3a6807595460ecd6b7f97f85c53625ce763
dakota <40772063+irradiated@users.noreply.github.com>
2023-11-29 23:04:32 -0800

    sh script

--- database/testing/regression/insertion_regression_test.sh
+++ database/testing/regression/insertion_regression_test.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# function to compare actual and expected outputs
-compare_outputs() 
-{
-    actual_output="$1"
-    expected_output="$2"
+# list of unittest scripts, to test more scripts add their path to the list
+unittest_scripts=("company_statements_UnitTest.py" "realtime_stocks_UnitTest.py")
 
-    if cmp -s "$actual_output" "$expected_output"; then
-        echo "Test passed: $actual_output matches $expected_output"
-    else
-        echo "Test failed: "
-    fi
+# run unittest code and print results
+run_unittest() 
+{
+  script_name=$1
+  python "$script_name"
+  # $? is an env var that holds the exit code of the last run command
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
+    echo "$script_name passed successfully."
+  else
+    echo "$script_name failed."
+    exit 1
+  fi
 }
 
-# run tests
-run_tests()
-{
-    test_program="$1"
-    expected_output="$2"
-    output_file="results_$test_program.txt"
+# iterate over scripts and run each
+for script in "${unittest_scripts[@]}"; do
+  run_unittest "$script"
+done
 
-    ./"$test_program" > "$output_file"
-    compare_outputs "$output_file" "$expected_output"
-}
+echo "All scripts passed successfully."
+exit 0
