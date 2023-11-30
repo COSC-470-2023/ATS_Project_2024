b261379f14579c4129974c8cab11fc4eddc1bcbd
Noah Blake <94071041+Badwolf202@users.noreply.github.com>
2023-11-29 23:00:27 -0800

    MORE UNIT TEST

--- database/testing/regression/stocks_UnitTest.py
+++ database/testing/regression/stocks_UnitTest.py
@@ -1,6 +1,6 @@
 import connect
 import unittest
-from realtime_stock_insert import execute_insert, get_company_id, load_output_file
+from realtime_stock_insert import execute_insert, get_company_id, load_output_file, check_keys
 
 realtime_data = load_output_file("database/testing/regression/stocks_test.json")
 class StockInsertion(unittest.TestCase):  
@@ -8,6 +8,22 @@ class StockInsertion(unittest.TestCase):
         with connect.connect() as conn:
             for entry in realtime_data:
                 get_company_id(entry, conn)
+
+
+    def test_check_keys(self):
+        # mock entry missing a lot of keys
+        entry = {
+            "_realtime_date": "2023-01-01",
+            "_realtime_price": 100.0,
+        }
+
+        # call the function
+        keys = check_keys(entry)
+
+        # verify that missing keys are assigned a value of None
+        for key, value in keys.items():
+            if key not in entry:
+                self.assertEqual(value, None)
 
 
     def testInsertion(self):
