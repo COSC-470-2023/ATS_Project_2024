25064310f118950fa72238e29958add0b1738634
jakefischer19 <98055934+jakefischer19@users.noreply.github.com>
2023-12-05 18:28:50 -0800

    refactored methods in index insertion

--- database/processing/historical_index_insert.py
+++ database/processing/historical_index_insert.py
@@ -5,7 +5,7 @@ from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
 # Globals
-OUTPUT_FILE_PATH = "./SMF_Project_2023/data_collection/output/historical_index_output.json"
+OUTPUT_FILE_PATH = "./data_collection/output/historical_index_output.json"
 
 def load_output_file(path):
     try:
@@ -20,45 +20,52 @@ def load_output_file(path):
         exit(1)
         
 def execute_insert(connection, entry, index_id):
-        date = entry['_historical_date']
-        open = entry['_historical_open']
-        high = entry['_historical_high']
-        low = entry['_historical_low']
-        close = entry['_historical_close']
-        adj_close = entry['_historical_adjClose']
-        volume = entry['_historical_volume']
-        unadjusted_volume = entry['_historical_unadjustedVolume']
-        change = entry['_historical_change']
-        change_percentage = entry['_historical_changePercent']
-        vwap = entry['_historical_vwap']
-        change_over_time = entry['_historical_changeOverTime']
-        # Excute row insertion
-        connection.execute(
-            text(
-                f"INSERT INTO `historical_index_values` VALUES ('{index_id}', '{date}', '{open}', '{high}', '{low}', '{close}','{adj_close}', '{volume}', '{unadjusted_volume}', '{change}', '{change_percentage}', '{vwap}', '{change_over_time}')"
-            )
-        )
+    keys = [    
+        '_historical_date',
+        '_historical_open',
+        '_historical_high',
+        '_historical_low',
+        '_historical_close',
+        '_historical_adjClose',
+        '_historical_volume',
+        '_historical_unadjustedVolume',
+        '_historical_change',
+        '_historical_changePercent',
+        '_historical_vwap',
+        '_historical_changeOverTime',
+    ]
+
+    # get key value, assign value to key. if key doesn't exist, assign value of None
+    row = {key: entry.get(key, None) for key in keys}
+
+    # append generated id
+    row["index_id"] = index_id
+
+    query = text("INSERT INTO `historical_index_values` VALUES (:index_id, :_historical_date, :_historical_open, :_historical_high, :_historical_low, :_historical_close, :_historical_adjClose, :_historical_volume, :_historical_unadjustedVolume, :_historical_change, :_historical_changePercent, :_historical_vwap, :_historical_changeOverTime)")
+        
+    # Excute row insertion
+    connection.execute(statement=query, parameters=row)
 
 # Used to get id associated with an index        
 def get_index_id(entry, connection):
-    symbol = entry['_historical_symbol']
-    name = entry['_historical_name']
-    id_query = f"SELECT id FROM `indexes` WHERE symbol = '{symbol}'"
+    # set query parameters
+    params = {"symbol": entry["_historical_symbol"], "name": entry["_historical_name"]}
+
+    id_query = text("SELECT id FROM `indexes` WHERE symbol = :symbol")
     # check if index exists in indexes table
-    result = connection.execute(text(id_query))
+    result = connection.execute(text("SELECT id FROM `indexes` WHERE symbol = :symbol"), parameters=params)
+    
     row = result.one_or_none()
+
     if row is None:
-        # execute plain sql insert statement - trigger will fire
-        connection.execute(
-            text(
-                f"INSERT INTO `indexes`(`indexName`, `symbol`) VALUES ('{name}', '{symbol}')"
-            )
-        )
-        connection.commit()
+        # if index doesn't exist, create new row in indexes table - trigger generates new ID
+        connection.execute(text("INSERT INTO `indexes` (`indexName`, `symbol`) VALUES (:name, :symbol)") ,parameters=params)
+    
         # get id generated from trigger
-        result = connection.execute(text(id_query)) 
+        result = connection.execute(id_query, parameters=params) 
         index_id = result.one()[0]    
     else:
+        # if the index exists, fetch the existing ID
         index_id = row[0]
     return index_id
 
@@ -68,17 +75,22 @@ def main():
         historical_data = load_output_file(OUTPUT_FILE_PATH)
         # create with context manager
         with connect.connect() as conn:
-            for entry in historical_data:
-                index_id = get_index_id(entry, conn)
-                try:
-                    # Excute row insertion
-                    execute_insert(conn,entry,index_id)
-                except SQLAlchemyError as e:
-                    # catch base SQLAlchemy exception, print SQL error info, then continue to prevent silent rollbacks
-                    print(f"Error: {e}")
-                    continue
-            # Commit changes to database (otherwise it rolls back)
-            conn.commit()  
+            # begin transaction with context manager, implicit commit on exit or rollback on exception
+            with conn.begin():
+                for entry in historical_data:
+                    if isinstance(entry, dict):
+                        index_id = get_index_id(entry, conn)
+                        try:
+                            # Excute row insertion
+                            execute_insert(conn,entry,index_id)
+                        except SQLAlchemyError as e:
+                            # catch base SQLAlchemy exception, print SQL error info, then continue to prevent silent rollbacks
+                            print(f"Error: {e}")
+                            continue
+                    else:
+                        # entry is not a dictionary, skip it
+                        continue
+
                 
     except Exception as e:
         print(traceback.format_exc())
