5cb8ef9f4e8277951e862f3bd1bffbec7e5263b1
dakota <40772063+irradiated@users.noreply.github.com>
2023-12-06 19:46:57 -0800

    Update realtime_commodity_insert.py

--- database/processing/realtime_commodity_insert.py
+++ database/processing/realtime_commodity_insert.py
@@ -19,54 +19,64 @@ def load_output_file(path):
     except json.JSONDecodeError:
         print(f"Error decoding JSON in '{path}'")
         exit(1)
-        
+
+def check_keys(entry):
+    # list of keys expected to be committed
+    keys = [
+        "_realtime_symbol",
+        "_realtime_name",
+        "_realtime_price",
+        "_realtime_changePercent",
+        "_realtime_change",
+        "_realtime_dayLow",
+        "_realtime_dayHigh",
+        "_realtime_yearHigh",
+        "_realtime_yearLow",
+        "_realtime_mktCap",
+        "_realtime_exchange",
+        "_realtime_volume",
+        "_realtime_volAvg",
+        "_realtime_open",
+        "_realtime_prevClose",
+        "_realtime_eps",
+        "_realtime_pe",
+        "_realtime_earningsAnnouncement",
+        "_realtime_sharesOutstanding",
+        "_realtime_date",
+    ]
+
+    # get key value, assign value to key. if key doesn't exist, assign value of None
+    return {key: entry.get(key, None) for key in keys}
+
 def execute_insert(connection, entry, commodity_id):
-    # Declare and initialize variables
-    date = entry['_realtime_date'] 
-    price = entry['_realtime_price']
-    change_percentage = entry['_realtime_changePercent']
-    change = entry['_realtime_change']
-    day_low = entry['_realtime_dayLow']
-    day_high = entry['_realtime_dayHigh']
-    year_high = entry['_realtime_yearHigh']
-    year_low = entry['_realtime_yearLow']
-    market_cap = entry['_realtime_mktCap']
-    if market_cap is None:
-        market_cap = "NULL"
-    else:
-        market_cap = "'" + market_cap + "'"
-    exchange = entry['_realtime_exchange']
-    volume = entry['_realtime_volume']
-    volume_avg = entry['_realtime_volAvg']
-    open = entry['_realtime_open']
-    prev_close = entry['_realtime_prevClose']
-    
-      # Execute row insertion
-    connection.execute(
-        text(
-            f"INSERT INTO `realtime_commodity_values` VALUES ('{commodity_id}', '{date}', '{price}', '{change_percentage}', '{change}', '{day_low}', '{day_high}', '{year_high}', '{year_low}', {market_cap}, '{exchange}', '{volume}', '{volume_avg}', '{open}', '{prev_close}')"
-        )
-    )
+    # check for missing keys, assign them values of None
+    row = check_keys(entry)
+
+    # append generated id to dict
+    row["commodity_id"] = commodity_id
+
+    # parameterized query
+    query = text("INSERT INTO `realtime_commodity_values` VALUES (:commodity_id, :_realtime_symbol, :_realtime_name, :_realtime_price, :_realtime_changePercent, :_realtime_change, :_realtime_dayLow, :_realtime_dayHigh, :_realtime_yearHigh, , :_realtime_yearLow, :_realtime_mktCap, :_realtime_exchange, :_realtime_volume, :_realtime_volAvg, :_realtime_open, :_realtime_prevClose, :_realtime_eps, :_realtime_pe, :_realtime_earningsAnnouncement, :_realtime_sharesOutstanding, :_realtime_date)")
+
+    # dict unpacked into query :parameters by matching key to parameter name
+    connection.execute(statement=query, parameters=row)
     
 def get_commodity_id(entry, connection):
-    #Declare and initalize variables
-    symbol = entry['_realtime_symbol']
-    name = entry['_realtime_name']
-    id_query = f"SELECT id FROM `commodities` WHERE symbol = '{symbol}'"
-    
-    # Check if commodity exists in commodities table
-    result = connection.execute(text(id_query))
+    # create dict of parameters to pass
+    params = {"symbol": entry["_realtime_symbol"], "name": entry["_realtime_name"]}
+
+    # query to fetch id
+    id_query = text("SELECT id FROM `commodities` WHERE symbol = ':_realtime_symbol'")
+
+    # check if commodity exists in commodities table already
+    result = connection.execute(id_query, parameters=params)
     row = result.one_or_none()
 
     if row is None:
         # if commodity doesn't exist, create new row in commodites table - trigger generates new ID
-        connection.execute(
-            text(
-                f"INSERT INTO `commodities`(`commodityName`, `symbol`) VALUES ('{name}', '{symbol}')"
-            )
-        )
+        connection.execute(text("INSERT INTO `commodities`(`commodityName`, `symbol`) VALUES (:_realtime_name, :_realtime_symbol)"), parameters=params)
         # get the generated ID
-        result = connection.execute(text(id_query))
+        result = connection.execute(id_query, parameters=params)
         commodity_id = result.one()[0]
     else:
         # if the company exists, fetch the existing ID
@@ -78,23 +88,23 @@ def main():
     realtime_data = load_output_file(OUTPUT_FILE_PATH)
 
     try:
-        # create with context manager, implicit commit on close
+        # create connection with context manager, closed on exit
         with connect.connect() as conn:
-            for entry in realtime_data:
-                if bool(entry):
-                    commodity_id = get_commodity_id(entry, conn)
-                    try:
-                        # process realtime data
-                        execute_insert(conn, entry, commodity_id)
-                    except SQLAlchemyError as e:
-                        # catch base SQLAlchemy exception, print SQL error info, then continue to prevent silent rollbacks
-                        print(f"Error: {e}")
-                        continue
-                else:
-                    continue
-                    
-            # Commit changes to database (otherwise it rolls back)
-            conn.commit()       
+            # begin transaction with context manager, implicit commit on exit
+            with conn.begin():
+                for entry in realtime_data:
+                    if isinstance(entry, dict):
+                        commodity_id = get_commodity_id(entry, conn)
+                        try:
+                            # process realtime data
+                            execute_insert(conn, entry, commodity_id)
+                        except SQLAlchemyError as e:
+                            # catch base SQLAlchemy exception, print SQL error info, then continue to prevent silent rollbacks
+                            print(f"Error: {e}")
+                            continue
+                    else:
+                        # entry is not a dictionary, skip it
+                        continue   
     except Exception as e:
         print(traceback.format_exc())
         print(f"SQL connection error: {e}")
