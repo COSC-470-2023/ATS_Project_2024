183f54c5e8372a13760f08f1ced2355a1c36a946
Alan Abdollahzadeh <alan.abdollahzadeh@gmail.com>
2024-04-07 16:09:15 -0700

    Only include most recent symbol changes

--- ats/database/symbol_change_update.py
+++ ats/database/symbol_change_update.py
@@ -11,6 +11,18 @@ connection_manager = db_handler.ConnectionManager.instance()
 logger = Logger.instance()
 
 
+def prune_old_data(connection, data):
+    pruned_data = []
+    query = sqlalchemy.text("SELECT symbol FROM companies")
+    result = connection.execute(query)
+    symbols = result.all()
+    for symbol in symbols:
+        for entry in data:
+            if symbol == entry['_change_oldSymbol']:
+                pruned_data.append(entry)
+    return pruned_data
+
+
 def update_symbol(connection, symbol):
     try:
         # Variable Declarations
@@ -31,7 +43,8 @@ def main():
     try:
         with connection_manager.connect() as connection:
             data = file_handler.read_json(globals.FN_OUT_SYMBOL_CHANGE)
-            for entry in data:
+            pruned_data = prune_old_data(connection, data)
+            for entry in pruned_data:
                 update_symbol(connection, entry)
             connection.commit()
     except Exception as e:
