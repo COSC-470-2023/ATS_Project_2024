388255971c42b9d8151e566043660eba73ee0e7c
JRawlings66 <jacobrawlings66@gmail.com>
2024-04-15 17:55:33 -0700

    Added comments to api_handler.py and data_handler.py

--- ats/util/data_handler.py
+++ ats/util/data_handler.py
@@ -14,18 +14,36 @@ MappingCallback: TypeAlias = Callable[[Entry], ...]
 
 
 class Mapping:
+    """
+    A class to manage field mappings, allowing transformation of raw data.
+    """
     def __init__(self):
         self.mapping = {}
 
     def add(self, field: str, callback: MappingCallback):
+        """
+        Adds a mapping from a data field to a callback function that processes data for that field.
+        :param field: The field name for the mapping to apply.
+        :param callback: callback function that will process data.
+        """
         self.mapping[field] = callback
 
     def lookup(self, field: str, *args):
+        """
+        Executes the callback tied to the given field.
+        :param field: The given field name.
+        :return: result of the callback function.
+        """
         cb = self.mapping[field]
         return cb(*args)
 
 
 def mapping_callback(cb: Callable[[Kwargs], ...]) -> MappingCallback:
+    """
+    Decorator to transform a simple function into a MappingCallback.
+    :param cb: The callback function that processes the data.
+    :return: A wrapper function.
+    """
     def wrapper(entry: Entry = None):
         kwargs = {
             ENTRY: entry,
@@ -39,7 +57,15 @@ def process_raw_data(raw_data: list[dict],
                      non_api_fields: list[str] = None,
                      mapping: Mapping = None,
                      entry_key: str = None) -> list[dict]:
-    # TODO: write docstring
+    """
+    Processes a list of raw data entries according to specified field and non-api-field mappings.
+    :param raw_data: A JSON representing raw data entries.
+    :param api_fields: A dictionary mapping raw data fields to processed data fields.
+    :param non_api_fields: A list of fields not provided by the API.
+    :param mapping: A Mapping object for non-API fields.
+    :param entry_key: Optional key to handle nested data structures.
+    :return: A list of processed dictionaries.
+    """
     data = []
 
     def helper(raw_entry_sub: dict = None):
@@ -65,7 +91,16 @@ def process_entry(raw_entry: dict,
                   non_api_fields: list[str] = None,
                   mapping: Mapping = None,
                   raw_entry_sub: dict = None):
-    # TODO: write docstring
+    """
+    Processes an individual entry from raw data.
+
+    :param raw_entry: The dictionary containing raw data.
+    :param api_fields: A dictionary mapping raw data fields to their new keys.
+    :param non_api_fields: A list of fields not provided by the API, that must be Mapped.
+    :param mapping: A Mapping object for non-api-fields.
+    :param raw_entry_sub: Optional dictionary for nested data.
+    :return: A dictionary of the processed entry.
+    """
     processed_entry = {}
 
     for field_name in api_fields:
